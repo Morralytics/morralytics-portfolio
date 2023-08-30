46793a8aa6e3b02a27613f182430b24fc98b310b
Nick Morris <nicholasmorris2599@gmail.com>
2023-08-30 16:11:22 -0700

    Setup for navigation drop down bar appearing on small screen sizes.

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+'use client'
+
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Nav = () => {
+    const router = useRouter();
+    const [toggerDropdown, setToggerDropdown] = useState(false);
+
   return (
-    <div>Nav</div>
+    <nav>
+        {/* Logo image that links back to the main page */}
+
+
+        {/* Mobile nav bar as a dropdown */}
+        <div className='sm:hidden'>
+            Drop
+        </div>
+    </nav>
   )
 }
 
