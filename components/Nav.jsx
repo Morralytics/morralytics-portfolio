4cf90a85e542a078bb9e56a72a52d0fa56314807
Nick Morris <nicholasmorris2599@gmail.com>
2023-08-30 18:29:02 -0700

    Styles added to fix the dropdown box.

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,62 +1,51 @@
-'use client'
+"use client";
 
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import Link from 'next/link';
-import Image from 'next/image';
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
 
-import { Bars3Icon } from '@heroicons/react/24/solid'
+import { Bars3Icon } from "@heroicons/react/24/solid";
 
 const Nav = () => {
-    const router = useRouter();
-    const [toggleDropdown, setToggleDropdown] = useState(false);
+  const router = useRouter();
+  const [toggleDropdown, setToggleDropdown] = useState(false);
 
   return (
-    <nav className='flex justify-between w-full mb-16 pt-3 px-5'>
+    <nav className=" w-full mb-16 pt-3 px-5">
+      <section className="flex justify-between">
         {/* Logo image that links back to the main page */}
-        <Link
-            href='/'
-        >
-            <Image 
-                src='/assets/icons/PortfolioLogo.png'
-                alt='Portfolio Logo'
-                width={30}
-                height={30}
-            />
+        <Link href="/">
+          <Image
+            src="/assets/icons/PortfolioLogo.png"
+            alt="Portfolio Logo"
+            width={40}
+            height={40}
+          />
         </Link>
 
         {/* Mobile nav bar as a dropdown */}
-        <div className='relative sm:hidden'>
-            <div className='flex'>
-                <Bars3Icon 
-                    className='h-6 w-6'
-                    onClick={() => setToggleDropdown((prev) => !prev)}
-                />
-            
-            {toggleDropdown && (
-                <div className='absolute right-0 top-full mt-3 w-full p-5 rounded-lg min-w-[210px] flex flex-col gap-2 justify-end items-end'>
-                    <Link
-                        href='#about-me'
-                    >
-                        About me</Link>
-                    <Link
-                        href='#tech-stack'
-                    >
-                        Tech Stack</Link>
-                    <Link
-                        href='#projects'
-                    >
-                        Projects</Link>
-                    <Link
-                        href='#contact'
-                    >
-                        Contact</Link>
-                </div>
-            )}
-            </div>
+        <div className="sm:hidden">
+          <div className="flex flex-col items-end">
+            <Bars3Icon
+              className="h-8 w-8 "
+              onClick={() => setToggleDropdown((prev) => !prev)}
+            />
+          </div>
         </div>
+      </section>
+      <section className="absolute w-full pr-10">
+        {toggleDropdown && (
+          <div className="mt-3 p-5 w-full bg-persion-orange rounded-lg flex flex-col gap-2 items-start">
+            <Link href="#about-me">About me</Link>
+            <Link href="#tech-stack">Tech Stack</Link>
+            <Link href="#projects">Projects</Link>
+            <Link href="#contact">Contact</Link>
+          </div>
+        )}
+      </section>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
