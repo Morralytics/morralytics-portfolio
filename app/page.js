de846d0cc4d0a233f7fad8a59f352b4e1775865f
Nick Morris <nicholasmorris2599@gmail.com>
2023-09-04 13:32:43 -0700

    Seperated the items into two seperate sections to align with flex properly.

--- app/page.js
+++ app/page.js
@@ -2,18 +2,22 @@ import Image from "next/image";
 
 const page = () => {
   return (
-    <section className='w-full flex flex-col items-center justify-center'>
-      <h1 className="text-2xl">Hello and welcome!</h1>
-      <p>I'm Nick Morris</p>
-      <p>A Full Stack Web Developer</p>
-      <Image 
-        src='/assets/images/Me_BW_Adjusted.png'
-        alt="Profile Picture"
-        width={240}
-        height={60}
-        className="rounded-full"
-       
-      />
+    <section className='w-full flex flex-col'>
+      <div className="flex flex-col items-center">
+        <h1 className="text-2xl">Hello and welcome!</h1>
+        <p>I'm Nick Morris</p>
+        <p>A Full Stack Web Developer</p>
+      </div>
+      <div className="flex justify-end">
+        <Image
+          src='/assets/images/Me_BW_Adjusted.png'
+          alt="Profile Picture"
+          width={240}
+          height={240}
+          className="rounded-full"
+        
+        />
+      </div>
     </section>
   )
 }
