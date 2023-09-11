dcb884a10d6fc1c9ac1fc0e2ad9a188773867d78
Nick Morris <nicholasmorris2599@gmail.com>
2023-09-11 16:43:57 -0700

    Adding styles to the tech stack section.

--- components/TechStack.jsx
+++ components/TechStack.jsx
@@ -1,4 +1,4 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHtml5 } from "@fortawesome/free-brands-svg-icons";
 import { faCss3Alt } from "@fortawesome/free-brands-svg-icons";
 import { faReact } from "@fortawesome/free-brands-svg-icons";
@@ -7,14 +7,14 @@ import { faFigma } from "@fortawesome/free-brands-svg-icons";
 
 const TechStack = () => {
   return (
-    <div>
-        <FontAwesomeIcon icon={faHtml5} />
-        <FontAwesomeIcon icon={faCss3Alt} />
-        <FontAwesomeIcon icon={faReact} />
-        <FontAwesomeIcon icon={faEnvira} />
-        <FontAwesomeIcon icon={faFigma} />
-      </div>
-  )
-}
+    <div className="flex w-full justify-around items-center mt-16 py-8 w-full bg-[#F7F7F8] px-5 sm:px-16 sm:mt-28 lg:px-56">
+      <FontAwesomeIcon icon={faHtml5} className="h-16"/>
+      <FontAwesomeIcon icon={faCss3Alt} className="h-16"/>
+      <FontAwesomeIcon icon={faReact} className="h-16"/>
+      <FontAwesomeIcon icon={faEnvira} className="h-16"/>
+      <FontAwesomeIcon icon={faFigma} className="h-16"/>
+    </div>
+  );
+};
 
-export default TechStack
+export default TechStack;
