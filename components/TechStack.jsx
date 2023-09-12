10cd136d847825c8620c0fdd7ff42873522964ae
Nick Morris <nicholasmorris2599@gmail.com>
2023-09-11 19:53:59 -0700

    Moved icons into new tech stack component.

--- components/TechStack.jsx
+++ components/TechStack.jsx
@@ -7,13 +7,18 @@ import { faFigma } from "@fortawesome/free-brands-svg-icons";
 
 const TechStack = () => {
   return (
-    <div className="flex w-full justify-around items-center mt-16 py-8 w-full bg-[#F7F7F8] px-5 sm:px-16 sm:mt-28 lg:px-56">
-      <FontAwesomeIcon icon={faHtml5} className="h-16"/>
-      <FontAwesomeIcon icon={faCss3Alt} className="h-16"/>
-      <FontAwesomeIcon icon={faReact} className="h-16"/>
-      <FontAwesomeIcon icon={faEnvira} className="h-16"/>
-      <FontAwesomeIcon icon={faFigma} className="h-16"/>
-    </div>
+    <section>
+      <div className="flex w-full justify-around items-center mt-16 py-8 w-full bg-[#F7F7F8] px-5 sm:px-16 sm:mt-28 lg:px-56">
+        <FontAwesomeIcon icon={faHtml5} className="h-16" />
+        <FontAwesomeIcon icon={faCss3Alt} className="h-16" />
+        <FontAwesomeIcon icon={faReact} className="h-16" />
+        <FontAwesomeIcon icon={faEnvira} className="h-16" />
+        <FontAwesomeIcon icon={faFigma} className="h-16" />
+      </div>
+      <div>
+        
+      </div>
+    </section>
   );
 };
 
