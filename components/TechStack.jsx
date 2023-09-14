2a0bc0848a7e8646546f5c7a59102725193840e3
Nick Morris <nicholasmorris2599@gmail.com>
2023-09-14 16:16:22 -0700

    Basic structure built from figma concept. Need to polish.

--- components/TechStack.jsx
+++ components/TechStack.jsx
@@ -15,8 +15,54 @@ const TechStack = () => {
         <FontAwesomeIcon icon={faEnvira} className="h-16" />
         <FontAwesomeIcon icon={faFigma} className="h-16" />
       </div>
-      <div>
-        
+      <div className="flex w-full flex-col bg-white">
+
+
+        <div className="text-center text-black text-6xl font-bold">
+          Tech Stack
+        </div>
+
+        <div className="">
+          <div className="">
+            <div className="" />
+            <div className="text-center text-black text-2xl font-normal underline">
+              Web Structure and Design
+            </div>
+          </div>
+          <div className="text-center text-black text-xl font-light">
+            Skilled in HTML5 and CSS3, I excel at crafting visually engaging and
+            responsive web designs using up-to-date coding techniques.
+          </div>
+        </div>
+
+
+        <div className="">
+          <div className="">
+            <div className="text-center text-black text-2xl font-normal underline">
+              Data and Organization
+            </div>
+          </div>
+          <div className="text-center text-black text-xl font-light">
+            For data management, I excel with MongoDB, ensuring efficient
+            organization, and for collaborative design, I leverage Figma.
+          </div>
+        </div>
+
+
+        <div className="">
+          <div className="">
+            <div className="text-center text-black text-2xl font-normal underline">
+              Frontend Development
+            </div>
+          </div>
+          <div className="text-center text-black text-xl font-light">
+            Passionate about UI/UX, I specialize in React and Next.js, creating
+            high-performance web applications with cutting-edge tools and
+            technologies.
+          </div>
+        </div>
+
+
       </div>
     </section>
   );
