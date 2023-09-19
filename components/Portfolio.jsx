1907d777f72d40e7bfc177201426b13531e2fc5d
Nick Morris <nicholasmorris2599@gmail.com>
2023-09-18 17:42:56 -0700

    Submitting edit video to test on mobile.

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -4,7 +4,7 @@ const Portfolio = () => {
   return (
     <section>
       <div className="w-full flex flex-col mt-14 px-5 sm:px-16 sm:mt-28 lg:px-56">
-        <div>
+        <div className="flex flex-col">
           <div className="w-full text-black text-5xl font-bold">
             My Work Portfolio
           </div>
@@ -23,9 +23,11 @@ const Portfolio = () => {
         </div>
         <div>
           <video
-            src="/assets/videos/GlazedAndConfusedVideo.mp4"
-            autoplay=''
+            className="mt-12"
+            src="/assets/videos/Edits.mp4"
+            autoplay=""
             muted
+            defaultMuted
           />
         </div>
         <img src="/" />
