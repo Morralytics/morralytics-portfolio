da36a519bea55f35009a1dfecdf89328fad1137f
Nick Morris <nicholasmorris2599@gmail.com>
2023-10-10 15:29:54 -0700

    Basic structure completted of mobile contact page.

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -33,25 +33,33 @@ const Contact = () => {
         <div className="text-center text-black text-5xl font-bold mb-16">
           Contact Me
         </div>
-        
+
         {/* Mobile */}
         <div className="flex flex-col w-full items-center sm:hidden">
           {/* Contact form */}
           <div className="flex flex-col w-full mb-8">
             <form className="flex flex-col">
-              <input type="text" placeholder="First Name" className="border w-full"/>
-              <input type="text" placeholder="Email" className="border"/>
-              <input type="textarea" placeholder="Message" className="border"/>
+              <input
+                type="text"
+                placeholder="First Name"
+                className="border w-full"
+              />
+              <input type="text" placeholder="Email" className="border w-full" />
+              <textarea placeholder="Message" rows="5" className="border w-full" />
             </form>
           </div>
           {/* Map + Extras */}
-          <div className="flex flex-col w-full px-3 mb-8 min-[675px]:min-h-[355px] min-[711px]:min-h-[325px] min-[768px]:min-h-[325px] sm:px-1 sm:min-h-[380px] md:min-h-[300px] min-[1083px]:min-h-[300px] min-[1099px]:min-h-[270px] min-[1110px]:min-h-[240px] min-[1204px]:min-h-[210px] min-[875px]:min-h-[296px] min-[886px]:min-h-[240px] min-[980px]:min-h-[200px] lg:min-h-[296px] min-[1300px]:min-h-[190px] min-[1388px]:min-h-[200px] sm:justify-start sm:mx-0">
+          <div className="flex flex-col w-full mb-8">
             <div className="text-center text-black text-2xl font-normal underline py-3">
-              Data and Organization
-            </div>
-            <div className="text-center text-black text-xl font-light pb-3">
-              In data management, I excel with MongoDB, ensuring efficient
-              organization, and for collaborative design, I leverage Figma.
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22936.895343512537!2d-121.32910965030777!3d44.060397544811586!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54b8c0ffa5d3d251%3A0x1088e7acc720d1b4!2sBend%2C%20OR!5e0!3m2!1sen!2sus!4v1696975263536!5m2!1sen!2sus"
+                width="300"
+                height="225"
+                allowfullscreen=""
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"
+                className="w-full"
+              ></iframe>
             </div>
           </div>
         </div>
@@ -61,9 +69,13 @@ const Contact = () => {
           {/* Contact form */}
           <div className="flex flex-col w-3/4 px-3 mb-8 min-[675px]:min-h-[355px] min-[711px]:min-h-[325px] min-[768px]:min-h-[325px] sm:px-1 sm:min-h-[380px] md:min-h-[300px] min-[875px]:min-h-[296px] min-[886px]:min-h-[240px] min-[980px]:min-h-[200px] min-[1083px]:min-h-[300px] min-[1099px]:min-h-[270px] min-[1110px]:min-h-[240px] min-[1204px]:min-h-[210px] lg:min-h-[296px] min-[1300px]:min-h-[190px] min-[1388px]:min-h-[200px] sm:mx-5">
             <form className="flex flex-col">
-              <input type="text" placeholder="First Name" className="border w-full"/>
-              <input type="text" placeholder="Email" className="border"/>
-              <input type="textarea" placeholder="Message" className="border"/>
+              <input
+                type="text"
+                placeholder="First Name"
+                className="border w-full"
+              />
+              <input type="text" placeholder="Email" className="border" />
+              <input type="textarea" placeholder="Message" className="border" />
             </form>
           </div>
           {/* Map + Extras */}
