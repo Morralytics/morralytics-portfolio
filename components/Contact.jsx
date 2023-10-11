812b3f5c7866140b93a65670524e9ba1e3286e69
Nick Morris <nicholasmorris2599@gmail.com>
2023-10-11 10:42:54 -0700

    Finished mobile version.

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -37,21 +37,22 @@ const Contact = () => {
         {/* Mobile */}
         <div className="flex flex-col w-full items-center sm:hidden">
           {/* Contact form */}
-          <div className="flex flex-col w-full mb-8">
+          <div className="flex flex-col w-full mb-2">
             <form className="flex flex-col last:items-end">
               <input
                 type="text"
                 placeholder="First Name"
-                className="border border-liver rounded-md text-liver w-full py-2 px-2 my-1.5"
+                className="border border-liver rounded-md text-liver w-full py-2 px-2 my-1.5 focus:outline-persion-orange"
               />
-              <input type="text" placeholder="Email" className="border border-liver rounded-md text-liver w-full py-2 px-2 my-1.5" />
-              <textarea placeholder="Message" rows="5" className="border border-liver rounded-md text-liver w-full py-2 px-2 my-1.5 resize-none" />
-              <input type="submit" className="rounded-md text-white w-1/4 py-2 px-2 my-1.5 bg-gradient-to-b from-persion-orange to-[#CDA584]"/>
+              <input type="text" placeholder="Email" className="border border-liver rounded-md text-liver w-full py-2 px-2 my-1.5 focus:outline-persion-orange" />
+              <textarea placeholder="Message" rows="5" className="border border-liver rounded-md text-liver w-full py-2 px-2 my-1.5 focus:outline-persion-orange resize-none" />
+              <input type="submit" className="rounded-md text-white w-1/4 py-2 px-2 my-1.5 bg-gradient-to-b from-persion-orange to-[#CDA584] hover:cursor-pointer"/>
             </form>
           </div>
           {/* Map + Extras */}
           <div className="flex flex-col w-full mb-8">
-            <div className="text-center text-black text-2xl font-normal underline py-3">
+            <div className="text-black font-semibold text-md py-3">
+              <p>LETS GET A COFFEE!</p>
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22936.895343512537!2d-121.32910965030777!3d44.060397544811586!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54b8c0ffa5d3d251%3A0x1088e7acc720d1b4!2sBend%2C%20OR!5e0!3m2!1sen!2sus!4v1696975263536!5m2!1sen!2sus"
                 width="300"
